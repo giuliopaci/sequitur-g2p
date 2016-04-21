5c6f07d40a097fc6d5634fc54bd7db630d7268bf
Giulio Paci <giuliopaci@gmail.com>
2016-04-21 14:03:08 +0200

    Fix some spelling errors in SimpleGoodTuring.py.

--- SimpleGoodTuring.py
+++ SimpleGoodTuring.py
@@ -33,7 +33,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, you will find it at
 http://www.gnu.org/licenses/gpl.html, or write to the Free Software
-Foundation, Inc., 51 Franlin Street, Fifth Floor, Boston, MA 02110,
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110,
 USA.
  
 Should a provision of no. 9 and 10 of the GNU General Public License
@@ -67,7 +67,7 @@ def zipfFit(data):
     """
     Takes a list of (count, count-of-count) pairs, and performs a
     linear least-squares fit in the log-log domain.  This meaningful
-    under the assumtion that the rank-frequency relation follows a
+    under the assumption that the rank-frequency relation follows a
     power law (known as Zipf's law).
     """
     data.sort()
