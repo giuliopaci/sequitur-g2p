889e0d597b264e4e47cdafeb18fc00c5362967ed
Giulio Paci <giuliopaci@gmail.com>
2016-04-21 14:03:08 +0200

    Fix some spelling errors in sequitur.py.

--- sequitur.py
+++ sequitur.py
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, you will find it at
 http://www.gnu.org/licenses/gpl.html, or write to the Free Software
-Foundation, Inc., 51 Franlin Street, Fifth Floor, Boston, MA 02110,
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110,
 USA.
  
 Should a provision of no. 9 and 10 of the GNU General Public License
@@ -150,7 +150,7 @@ class MixtureModel(Model):
 	self.discount = component.discount
 
 class MixtureModelComponent(object):
-    "for side-ways compatibility with branch micture-model"
+    "for side-ways compatibility with branch mixture-model"
     pass
 
 
